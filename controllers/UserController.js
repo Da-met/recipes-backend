5b58be8882c0af7b1c1878fe9f3a5e3be552c113
Da-met <dmetelskaya@yandex.ru>
2024-10-18 19:18:05 +0300

    commit

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -68,7 +68,7 @@ export const login = async (req, res) => {
     } catch (err) {
         console.log(err)
         res.status(500).json({
-            message: 'Не удалось зарегистрироваться!'
+            message: 'Не удлось зарегистрироваться!'
         });
     }
 };
