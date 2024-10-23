4acdfba1cc9d09d1d22213a14de4b11bec77a56a
Da-met <dmetelskaya@yandex.ru>
2024-10-23 19:25:52 +0300

    commit 13

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ app.get('/comments-every', CommentController.getEvery);
 app.delete('/comment/:id', CommentController.remove);
 app.delete('/comments/:id', CommentController.removeAll);
 
-// console.log(process.env) 
+console.log(process) 
 
 app.listen(process.env.MONGODB_URI || 3333, (err) => {
     if(err) {
